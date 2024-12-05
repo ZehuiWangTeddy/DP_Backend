3ebcca1e23e6e45273f0228b666c4bfa84f6bd0c
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2024-12-05 17:15:32 +0100

    Updated SubscriptionFactory to use Carbon for date manipulation and changed price to randomElement

--- backend/database/factories/SubscriptionFactory.php
+++ backend/database/factories/SubscriptionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subscription>
@@ -11,13 +12,16 @@ class SubscriptionFactory extends Factory
 {
     public function definition(): array
     {
+        $startDate = $this->faker->dateTimeBetween('-10 years', 'now');
+        $endDate = (clone $startDate)->modify('+1 year');
+
         return [
             'user_id' => \App\Models\User::factory(), // Relates to User
-            'price' => $this->faker->randomFloat(7.99, 10.99, 13.99),
+            'price' => $this->faker->randomElement([7.99, 10.99, 13.99]),
             'name' => $this->faker->randomElement(['SD', 'HD', 'UHD']),
             'status' => $this->faker->randomElement(['paid', 'expired']),
-            'start_date' => $this->faker->date(),
-            'end_date' => $this->faker->date(),
+            'start_date' => $startDate->format('Y-m-d'),
+            'end_date' => $endDate->format('Y-m-d'),
             'payment_method' => $this->faker->randomElement([
                 'PayPal',
                 'Visa',
