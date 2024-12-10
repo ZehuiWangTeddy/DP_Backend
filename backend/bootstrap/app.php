a7b6e1b96f94d26810ca7e5ce0bc51df464777af
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2024-12-10 12:47:16 +0100

    Updated app.php --FIX

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,13 +7,25 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
-        apiPrefix: '/',
+        apiPrefix: '/api/v1',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api([
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (\Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException $e) {
+            return response()->json([
+                'meta' => [
+                    'code' => 401,
+                    'message' => $e->getMessage(),
+                ],
+                'data' => [],
+            ], 401);
+        });
     })->create();
