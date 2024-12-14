188fa02b1f480ccc12075a1b235e185b6461a873
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2024-12-14 20:33:00 +0100

    modified movieController to aupport all request types --ADD

--- backend/app/Http/Controllers/MovieController.php
+++ backend/app/Http/Controllers/MovieController.php
@@ -8,9 +8,58 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     /**
+     * Validation rules for creating a movie
+     */
+    private function storeRules()
+    {
+        return [
+            'title' => 'required|string|max:100',
+            'duration' => ['required', 'regex:/^\d{2}:\d{2}:\d{2}$/'],
+            'release_date' => 'required|date',
+            'quality' => 'required|array',
+            'age_restriction' => 'required|integer|min:0',
+            'genre' => 'required|array',
+            'viewing_classification' => 'required|string',
+            'available_languages' => 'required|array',
+        ];
+    }
+
+    /**
+     * Validation rules for updating a movie
+     */
+    private function updateRules()
+    {
+        return [
+            'title' => 'sometimes|string|max:100',
+            'duration' => ['sometimes', 'regex:/^\d{2}:\d{2}:\d{2}$/'],
+            'release_date' => 'sometimes|date',
+            'quality' => 'sometimes|array',
+            'age_restriction' => 'sometimes|integer|min:0',
+            'genre' => 'sometimes|array',
+            'viewing_classification' => 'sometimes|string',
+            'available_languages' => 'sometimes|array',
+        ];
+    }
+
+    /**
+     * Encode array fields to JSON
+     */
+    private function encodeArrayFields($data)
+    {
+        if (isset($data['quality'])) {
+            $data['quality'] = json_encode($data['quality']);
+        }
+        if (isset($data['genre'])) {
+            $data['genre'] = json_encode($data['genre']);
+        }
+        if (isset($data['available_languages'])) {
+            $data['available_languages'] = json_encode($data['available_languages']);
+        }
+        return $data;
+    }
+
+    /**
      * Display a listing of the movies.
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
@@ -20,37 +69,18 @@ class MovieController extends Controller
 
     /**
      * Store a newly created movie in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'title' => 'required|string|max:100',
-            'duration' => ['required', 'regex:/^\d{2}:\d{2}:\d{2}$/'],
-            'release_date' => 'required|date',
-            'quality' => 'required|array',
-            'age_restriction' => 'required|integer|min:0',
-            'genre' => 'required|array',
-            'viewing_classification' => 'required|string',
-            'available_languages' => 'required|array',
-        ]);
-
-        // Encode array fields to JSON
-        $validated['quality'] = json_encode($validated['quality']);
-        $validated['genre'] = json_encode($validated['genre']);
-        $validated['available_languages'] = json_encode($validated['available_languages']);
-
+        $validated = $request->validate($this->storeRules());
+        $validated = $this->encodeArrayFields($validated);
+        
         $movie = Movie::create($validated);
         return response()->json($movie, 201);
     }
 
     /**
      * Display the specified movie.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
@@ -60,36 +90,13 @@ class MovieController extends Controller
 
     /**
      * Update the specified movie in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
         $movie = Movie::findOrFail($id);
-
-        $validated = $request->validate([
-            'title' => 'sometimes|string|max:100',
-            'duration' => ['sometimes', 'regex:/^\d{2}:\d{2}:\d{2}$/'],
-            'release_date' => 'sometimes|date',
-            'quality' => 'sometimes|array',
-            'age_restriction' => 'sometimes|integer|min:0',
-            'genre' => 'sometimes|array',
-            'viewing_classification' => 'sometimes|string',
-            'available_languages' => 'sometimes|array',
-        ]);
-
-        // Encode array fields to JSON if they are present
-        if (isset($validated['quality'])) {
-            $validated['quality'] = json_encode($validated['quality']);
-        }
-        if (isset($validated['genre'])) {
-            $validated['genre'] = json_encode($validated['genre']);
-        }
-        if (isset($validated['available_languages'])) {
-            $validated['available_languages'] = json_encode($validated['available_languages']);
-        }
+        
+        $validated = $request->validate($this->updateRules());
+        $validated = $this->encodeArrayFields($validated);
 
         $movie->update($validated);
         return response()->json($movie, 200);
@@ -97,9 +104,6 @@ class MovieController extends Controller
 
     /**
      * Remove the specified movie from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
