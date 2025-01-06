686a9d6a51170eeb2d4e19c4c0717b7919d68141
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2025-01-06 15:32:54 +0100

    Added the missing EpisodeController.php file and adjusted the quality atribue for movie and episode --FIX --ADD

--- backend/database/factories/EpisodeFactory.php
+++ backend/database/factories/EpisodeFactory.php
@@ -18,14 +18,17 @@ class EpisodeFactory extends Factory
         return [
             'season_id' => \App\Models\Season::factory(), // Relates to Season
             'episode_number' => $this->faker->numberBetween(1, 20), // Unique episode number within a season
-            'title' => $this->faker->sentence(6), 
-            'duration' => $this->faker->time('H:i:s'), 
-            'release_date' => $this->faker->date(), 
-            'quality' => $this->faker->randomElement(['SD', 'HD', 'UHD']), 
+            'title' => $this->faker->sentence(6),
+            'duration' => $this->faker->time('H:i:s'),
+            'release_date' => $this->faker->date(),
+            'quality' => json_encode($this->faker->randomElements(
+                ['SD', 'HD', 'UHD'],
+                $this->faker->numberBetween(1, 3) // Choose between 1 and 3 qualities
+            )),
             'available_languages' => json_encode($this->faker->randomElements(
                 ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese'],
                 $this->faker->numberBetween(2, 4)
-            )), 
+            )),
             'viewing_classification' => ViewingClassificationHelper::determineViewingClassification($age), // Viewing classification based on age
         ];
     }
