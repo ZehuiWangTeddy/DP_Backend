66839f3aa530c2aa09a9f02d905b5a27d4eef405
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2025-01-06 15:39:27 +0100

    Made changes so each episode of a season gets the same quality atribute --FIX

--- backend/database/factories/EpisodeFactory.php
+++ backend/database/factories/EpisodeFactory.php
@@ -10,26 +10,34 @@ use App\Helpers\ViewingClassificationHelper;
  */
 class EpisodeFactory extends Factory
 {
+    protected static array $seasonQualities = [];
+
     public function definition(): array
     {
-        // Generate the age restriction first
-        $age = $this->faker->randomElement([0, 6, 9, 12, 16, 18]);
+        $seasonId = \App\Models\Season::factory()->create()->id;
+
+        // If quality for this season doesn't exist yet, generate it
+        if (!isset(static::$seasonQualities[$seasonId])) {
+            static::$seasonQualities[$seasonId] = json_encode($this->faker->randomElements(
+                ['SD', 'HD', 'UHD'],
+                $this->faker->numberBetween(1, 3)
+            ));
+        }
 
         return [
-            'season_id' => \App\Models\Season::factory(), // Relates to Season
-            'episode_number' => $this->faker->numberBetween(1, 20), // Unique episode number within a season
+            'season_id' => $seasonId,
+            'episode_number' => $this->faker->numberBetween(1, 20),
             'title' => $this->faker->sentence(6),
             'duration' => $this->faker->time('H:i:s'),
             'release_date' => $this->faker->date(),
-            'quality' => json_encode($this->faker->randomElements(
-                ['SD', 'HD', 'UHD'],
-                $this->faker->numberBetween(1, 3) // Choose between 1 and 3 qualities
-            )),
+            'quality' => static::$seasonQualities[$seasonId], // Use the cached quality for this season
             'available_languages' => json_encode($this->faker->randomElements(
                 ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese'],
                 $this->faker->numberBetween(2, 4)
             )),
-            'viewing_classification' => ViewingClassificationHelper::determineViewingClassification($age), // Viewing classification based on age
+            'viewing_classification' => ViewingClassificationHelper::determineViewingClassification(
+                $this->faker->numberBetween(0, 18)
+            ),
         ];
     }
 }
